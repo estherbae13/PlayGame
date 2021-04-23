81113259305959268b5ece4f274b3142632da8f2
estherbae13 <estiebae13@hotmail.com>
2021-04-23 10:15:48 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace PlayGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playButton = new System.Windows.Forms.Button();
+            this.counterLabel = new System.Windows.Forms.Label();
+            this.startLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.AutoSize = true;
+            this.playButton.BackColor = System.Drawing.Color.Transparent;
+            this.playButton.FlatAppearance.BorderColor = System.Drawing.Color.Snow;
+            this.playButton.FlatAppearance.BorderSize = 4;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Pink;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Ebrima", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.Snow;
+            this.playButton.Location = new System.Drawing.Point(148, 248);
+            this.playButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(437, 115);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "Play Game";
+            this.playButton.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // counterLabel
+            // 
+            this.counterLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.counterLabel.AutoSize = true;
+            this.counterLabel.Font = new System.Drawing.Font("Ebrima", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counterLabel.ForeColor = System.Drawing.Color.Snow;
+            this.counterLabel.Location = new System.Drawing.Point(153, 402);
+            this.counterLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.counterLabel.Name = "counterLabel";
+            this.counterLabel.Size = new System.Drawing.Size(312, 45);
+            this.counterLabel.TabIndex = 1;
+            this.counterLabel.Text = "GAME STARTS IN 3";
+            this.counterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.BackColor = System.Drawing.Color.Transparent;
+            this.startLabel.Font = new System.Drawing.Font("Ebrima", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startLabel.ForeColor = System.Drawing.Color.Snow;
+            this.startLabel.Location = new System.Drawing.Point(236, 219);
+            this.startLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(207, 70);
+            this.startLabel.TabIndex = 2;
+            this.startLabel.Text = "START!";
+            this.startLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
+            this.ClientSize = new System.Drawing.Size(724, 626);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.counterLabel);
+            this.Controls.Add(this.playButton);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Play Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label counterLabel;
+        private System.Windows.Forms.Label startLabel;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading; //allows the use of Thread.Sleep()
+using System.Media; // add this to your using statements to play .wav sounds 
+
+namespace PlayGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //make counter invisible
+            counterLabel.Visible = false;
+
+            //make start invisible
+            startLabel.Visible = false;
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            //make button clicking sound
+            SoundPlayer clicking = new SoundPlayer(Properties.Resources.clicking);
+            clicking.Play();
+
+            //change background of colour of form and make button invisible and show countdown
+            this.BackColor = Color.RosyBrown;
+            playButton.Visible = false;
+            counterLabel.Visible = true;
+
+            //make screen show countdown from 3 to 2
+            Refresh();
+            Thread.Sleep(1000);
+            counterLabel.Text = "GAME STARTS IN 2";
+
+            //countdown from 2 to 1
+            SoundPlayer countdown = new SoundPlayer(Properties.Resources.countdown);
+            countdown.Play();
+            Refresh();
+            Thread.Sleep(1000);
+            counterLabel.Text = "GAME STARTS IN 1";
+
+            //change background colour and change text to START with sound!
+            countdown.Play();
+            Refresh();
+            Thread.Sleep(1000);
+            startLabel.Visible = true;
+            counterLabel.Visible = false;
+            SoundPlayer startt = new SoundPlayer(Properties.Resources.startt);
+            startt.Play();
+
+            //change background colour to green
+            this.BackColor = Color.LightGreen;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
